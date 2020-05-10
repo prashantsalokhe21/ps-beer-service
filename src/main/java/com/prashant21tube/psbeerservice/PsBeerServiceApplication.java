0b9203988511a970e61d9a40f932694821f544e0
Prashant Salokhe <p.salokhe21@gmail.com>
2020-05-11 00:09:40 +0530

    Initial Commit

---
+++ src/main/java/com/prashant21tube/psbeerservice/PsBeerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.prashant21tube.psbeerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsBeerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PsBeerServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/prashant21tube/psbeerservice/PsBeerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prashant21tube.psbeerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PsBeerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
