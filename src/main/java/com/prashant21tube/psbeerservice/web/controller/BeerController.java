03a41c9cca41104236d9afeed16dc6eae6339b17
Prashant Salokhe <p.salokhe21@gmail.com>
2020-05-16 19:59:06 +0530

    Created Beer Service Controller. closes #3

---
+++ src/main/java/com/prashant21tube/psbeerservice/web/controller/BeerController.java
@@ -0,0 +1,43 @@
+package com.prashant21tube.psbeerservice.web.controller;
+
+import com.prashant21tube.psbeerservice.web.model.BeerDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+/**
+ * Project: ps-beer-service
+ * Package: com.prashant21tube.psbeerservice.web.controller
+ * <p>
+ * User: lenovo
+ * Date: 5/16/2020
+ * Time: 2:13 PM
+ * <p>
+ * Created with IntelliJ IDEA
+ * To change this template use File | Settings | File Templates.
+ */
+@RestController
+@RequestMapping("/api/v1/beer")
+public class BeerController {
+
+    @GetMapping("/{beerId}")
+    public ResponseEntity<BeerDto> findBeerById(@PathVariable(name = "beerId") UUID beerId,
+                                            @RequestParam(name = "showInventoryOnHand", required = false) String showInventoryOnHand) {
+        //TODO impl
+        return new ResponseEntity<>(BeerDto.builder().build(), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity saneNewBeer(@RequestBody BeerDto beerObj) {
+        //TODO impl
+        return new ResponseEntity(HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{beerId}")
+    public ResponseEntity updateBeerById(@PathVariable(name = "beerId") UUID beerId, @RequestBody BeerDto beerObj) {
+        //TODO impl
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
+}
